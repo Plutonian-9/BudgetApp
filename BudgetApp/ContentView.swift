fedbde902da0f12e4fca7187288597f7b8a1b244
Sydney Chapman <PlutonianPotato@outlook.com>
2023-03-31 17:37:05 -0400

    finished tutorial. Added Graph

--- BudgetApp/ContentView.swift
+++ BudgetApp/ContentView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+//    var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+
     var body: some View {
         NavigationView {
             ScrollView {
@@ -17,6 +21,24 @@ struct ContentView: View {
                         .font(.title2)
                         .bold()
                     
+                    //MARK: Chart
+                    let data = transactionListVM.accumulateTransactions()
+                    
+                    if !data.isEmpty {
+                        let totalExpenses = data.last?.1 ?? 0
+                        
+                        CardView {
+                            VStack(alignment: .leading) {
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+                                LineChart()
+                            }
+                            .background(Color.systemBackground)
+                        }
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.8), Color.icon)))
+                        .frame(height: 300)
+                    }
+             
                     //MARK: Recent Transaction List
                     RecentTransactionList()
                 }
