be764c0f3fcb4493bec16f6120d0e503c1ae2e9f
Sydney Chapman <PlutonianPotato@outlook.com>
2023-03-31 07:03:35 -0400

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Sydney Chapman on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
